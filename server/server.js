4ccee2bbd72a90201d8d42d0e0d20a8610de6834
Omjannu <89771105+Om-jannu@users.noreply.github.com>
2023-02-07 20:48:34 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -16,7 +16,10 @@ app.use(cookieParser());
 app.use(bodyParser.json({ extended:true }));
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(cors());
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+}) 
 
 // all of your routes 
 app.use("/",homeRouter);
